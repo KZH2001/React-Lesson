61d63099688924b2905df2b6a9ac6e334756331b
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2024-12-23 00:36:19 +0630

    upload

---
+++ Day1/react/src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  const title = "Product Categories";
+  const categories = ["Electronics", "Jewelry", "Men's Clothing","Women's Clothing"];
+  return (
+  <div id="category-section" className="p-5">
+      <p className="text-2xl text-gray-500 mb-3">{title}</p>
+      <div>
+          {categories.map((category) => (
+              <button className="border border-black px-5 py-2 me-2">
+          {category}
+          </button>
+          ))}
+      </div>
+  </div>
+  
+  
+  );
+  
+  
+  }
